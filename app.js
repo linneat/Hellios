0173d7d9e558d3af6f46aa738727b000883ec617
linneat <51674082+linneat@users.noreply.github.com>
2021-04-15 12:34:24 +0200

    burger nav white background

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const navAppear = () => {
   const burgerMenu = document.querySelector(".burgerMenu");
   const nav = document.querySelector(".nav ul");
+  const content = document.querySelector(".content");
+    const bodyWrap = document.querySelector(".bodyWrap");
 
   burgerMenu.addEventListener("click", () => {
+    content.classList.toggle("nav-activeContent");
+    bodyWrap.classList.toggle("nav-active-body")
     nav.classList.toggle("nav-active");
+    nav.classList.toggle("nav-active li");
+    nav.classList.toggle("nav-active li button");
+   
   });
-}
+};
 
 navAppear();

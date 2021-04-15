a900b23f69c3ee3ff7859ebc882b918df795ba4c
linneat <51674082+linneat@users.noreply.github.com>
2021-04-15 13:01:14 +0200

    burger cross

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const navAppear = () => {
   const burgerMenu = document.querySelector(".burgerMenu");
   const nav = document.querySelector(".nav ul");
   const content = document.querySelector(".content");
-    const bodyWrap = document.querySelector(".bodyWrap");
+  const bodyWrap = document.querySelector(".bodyWrap");
 
   burgerMenu.addEventListener("click", () => {
+    bodyWrap.classList.toggle("nav-active-body");
     content.classList.toggle("nav-activeContent");
-    bodyWrap.classList.toggle("nav-active-body")
+    burgerMenu.classList.toggle("line-change");
     nav.classList.toggle("nav-active");
     nav.classList.toggle("nav-active li");
     nav.classList.toggle("nav-active li button");
-   
   });
 };
 

558d94c35b61b01afdd1839501e5bc06355d3f4d
linneat <51674082+linneat@users.noreply.github.com>
2021-04-15 10:59:58 +0200

    add nav tag and ul class to all pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const navAppear = () => {
   const burgerMenu = document.querySelector(".burgerMenu");
-  const nav = document.querySelector(".navUl");
+  const nav = document.querySelector(".nav ul");
 
   burgerMenu.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
